bf0efd4470bebdfdf22599193aa640d2559883ea
MrTea1337 <87360741+MrTea1337@users.noreply.github.com>
2024-08-15 21:21:26 +0700

    Update module_2_hard.py
    Измениk начало и конец диапазона range во втором цикле. И убрал все ненужные уже более проверки из него.

--- module_2_hard.py
+++ module_2_hard.py
@@ -4,13 +4,10 @@ import random
 def too_old_ciper(n):
     password = []
     for i in range(1, n):
-        for j in range(1, n):
-            if n % (i + j) == 0 and i != j:                             #Создание списка пар
+        for j in range(i + 1, n):
+            if n % (i + j) == 0:                                        #Создание списка пар
                 password += [i, j]
-                for k in range(0, len(password) - 1):
-                    if password[k] == j and password[k + 1] == i:       #Удаление дублей пар
-                        del password[len(password) - 2: len(password)]
-                        break
+
     str_password = ''
     for i in password:                                                  #Ковертация списка в строку
         str_password += str(i)
