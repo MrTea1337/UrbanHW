57a559fa2d8b87ccfe045805200ae61376c8e2e2
MrTea1337 <87360741+MrTea1337@users.noreply.github.com>
2024-10-07 22:39:03 +0700

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -1,9 +1,7 @@
-
-
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = (abs(len(pair[0]) - len(pair[1])) for pair in zip(first, second) if pair[0] != pair[1])
+first_result = (abs(len(pair[0]) - len(pair[1])) for pair in zip(first, second) if len(pair[0]) != len(pair[1]))
 second_result = (len(first[i]) == len(second[i]) for i in range(min(len(first), len(second))))
 
 print(list(first_result))
