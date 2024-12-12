f2b62bea370fe83e113bc903861f3c00e5380ccb
MrTea1337 <87360741+MrTea1337@users.noreply.github.com>
2024-12-12 20:19:46 +0700

    Update module_14_2.py

--- module_14/14_2/module_14_2.py
+++ module_14/14_2/module_14_2.py
@@ -13,22 +13,22 @@ age INTEGER,
 balance INTEGER NOT NULL
 )
 ''')
-# for i in range(1, 11):
-#     cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
-#                    (f"User{i}", f"example{i}@gmail.com", i * 10, 1000))
-#
-# for i in range(1, 11, 2):
-#     cursor.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, i))
-#
-# for i in range(1, 11, 3):
-#     cursor.execute("DELETE FROM Users WHERE id = ?", (i,))
-#
-# cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60, ))
-# users = cursor.fetchall()
-# for username, email, age, balance in users:
-#     print(f'Имя: {username} | Почта: {email} | Возраст: {age} | Баланс: {balance} ')
+for i in range(1, 11):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (f"User{i}", f"example{i}@gmail.com", i * 10, 1000))
 
-# cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
+for i in range(1, 11, 2):
+    cursor.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, i))
+
+for i in range(1, 11, 3):
+    cursor.execute("DELETE FROM Users WHERE id = ?", (i,))
+
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60, ))
+users = cursor.fetchall()
+for username, email, age, balance in users:
+    print(f'Имя: {username} | Почта: {email} | Возраст: {age} | Баланс: {balance} ')
+
+cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
 
 cursor.execute("SELECT COUNT(*) FROM Users")
 total_users = cursor.fetchone()[0]
