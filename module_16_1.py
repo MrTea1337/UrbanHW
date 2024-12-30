aaa586f0d0fae7751cfd8b87b46e59ef97f60a94
MrTea1337 <87360741+MrTea1337@users.noreply.github.com>
2024-12-30 16:54:34 +0700

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def get_main_page() -> str:
+    return "Главная страница"
+
+@app.get("/user/admin")
+async def get_admin_page() -> str:
+    return "Вы вошли как администратор"
+
+@app.get("/user/{user_id}")
+async def get_user_number(user_id: int) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
+
+@app.get("/user")
+async def get_user_info(username: str = 'Danil', age: int = '23') -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
