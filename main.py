a7f4f94d912e45d7d8fdd0fbbe3858cab876b1aa
MrTea1337 <87360741+MrTea1337@users.noreply.github.com>
2025-01-03 19:16:33 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1st program
+print(9**0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+a = 2 * 2 + 2
+print(a)
+b = 2 * (2 + 2)
+print(b)
+print(a == b)
+
+# 4th program
+x = '123.456'
+x = float(x)
+x = x * 10
+x = int(x)
+x = x % 10
+print(x)
