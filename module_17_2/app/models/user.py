8fbba9abe105e26f414437de31184b336590854d
MrTea1337 <87360741+MrTea1337@users.noreply.github.com>
2025-01-05 15:58:10 +0700

    Add files via upload

---
+++ module_17_2/app/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from module_17_2.app.backend.db import Base
+from sqlalchemy.orm import relationship
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    task = relationship('Task', back_populates='user')
+
+from sqlalchemy.sql.ddl import CreateTable
+print(CreateTable(User.__table__))
